20cfbd03ed1e14cf286c3752177bcada104dbb2d
DamyanBG <d.dimitrov.1988@abv.bg>
2025-04-25 17:33:49 +0300

    query current weather logic

---
+++ api_requests.py
@@ -0,0 +1,47 @@
+from httpx import AsyncClient, HTTPStatusError, RequestError
+
+from exceptions import WeatherstackAPIError
+
+WEATHERSTACK_BASE_URL = "http://api.weatherstack.com"
+
+
+async def _safe_request(
+    method: str,
+    url: str,
+    **kwargs: dict,
+) -> dict:
+    async with AsyncClient() as client:
+        try:
+            if method == "GET":
+                response = await client.get(url, **kwargs)
+            elif method == "POST":
+                response = await client.post(url, **kwargs)
+            else:
+                raise ValueError(f"Unsupported method: {method}")
+
+            response.raise_for_status()
+            return response.json()
+
+        except HTTPStatusError as e:
+            raise WeatherstackAPIError(
+                f"API error {e.response.status_code} at {url}: {e.response.text}"
+            ) from e
+
+        except RequestError as e:
+            raise WeatherstackAPIError(
+                f"Network error during request to {url}: {str(e)}"
+            ) from e
+
+        except Exception as e:
+            raise WeatherstackAPIError(
+                f"Unexpected error during request to {url}: {str(e)}"
+            ) from e
+
+
+async def get_current_weather(query: str, api_key: str) -> dict:
+    url = f"{WEATHERSTACK_BASE_URL}/current"
+    params = {
+        "access_key": api_key,
+        "query": query
+    }
+    return await _safe_request("GET", url, params=params)
--- main.py
+++ main.py
@@ -1,5 +1,45 @@
+from typing import Union
+
+from mcp.server.fastmcp import Context
+from mcp.types import CallToolResult, TextContent
+
+from config import mcp
+from exceptions import WeatherstackAPIError
+from api_requests import get_current_weather
+
+
+@mcp.tool()
+async def query_current_weather(
+    query: str, ctx: Context
+) -> Union[dict, CallToolResult]:
+    """
+    Gets the current weather for a location using the Weatherstack API.
+
+    Supported location identifiers:
+    - City name (e.g. "New York")
+    - ZIP code (UK, Canada, US) (e.g. "99501")
+    - Latitude,Longitude coordinates (e.g. "40.7831,-73.9712")
+    - IP address (e.g. "153.65.8.20")
+    - Special keyword "fetch:ip" to auto-detect requester IP
+
+    Returns:
+        A dictonary, containing the response.
+    """
+    api_key = ctx.request_context.lifespan_context.api_key
+
+    try:
+        data = await get_current_weather(query, api_key)
+    except WeatherstackAPIError as e:
+        return CallToolResult(
+            isError=True,
+            content=[TextContent(type="text", text=f"Weatherstack API Error {e}")],
+        )
+
+    return data
+
+
 def main():
-    print("Hello from weatherstack-mcp-server!")
+    mcp.run(transport="stdio")
 
 
 if __name__ == "__main__":
